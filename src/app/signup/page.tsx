928e8173513a7394d0224eb13db8e00c32eb902e
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-02-24 13:36:56 +0530

    signupLogin

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,52 @@
-export default function SignupPage(){
-    return(
-        <div>
-            <h1 className="text-center text-white text-2xl">Signup Page</h1>
-        </div>
-    )
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
+export default function SignupPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = async () => {};
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Signup</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input
+        className="p-2 border text-black border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="username"
+        type="text"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="username"
+      />
+
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border text-black border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="email"
+        type="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border text-black border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button onClick={onSignup} className="p-2 border text-white border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Signup here</button>
+      <Link href="/login">Visit Login Page</Link>
+    </div>
+  );
 }
